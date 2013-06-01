386c47a92d2368f0734128392849e5c92ee41294
Hans Gogia <hans.gogia@gmail.com>
2013-06-01 07:02:54 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gogiasales::Application.config.secret_token = 'c2c115902ea67969323f6fa48e29b9b2e5885ed03930b6af422fe900440a86517152289da2ea5a1f674e5dc4d16751403c126fd98d7bac885c215ba771fba145'
