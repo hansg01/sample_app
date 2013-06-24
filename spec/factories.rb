77f73beea1ad82923dff977543327ad297d03ec7
Hans Gogia <hans.gogia@gmail.com>
2013-06-25 00:01:43 +0530

    Finish user edit, delete, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
 	factory :user do
-		name     "Hans Gogia"
-		email    "contact@hansgogia.com"
+		sequence(:name)  { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com"}
 		password "foobar"
 		password_confirmation "foobar"
-	end
+		factory :admin do
+			admin true
+    	end
+    end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,3 +6,10 @@ def full_title(page_title)
     "#{base_title} | #{page_title}"
   end
 end
+def sign_in(user)
+	visit signin_path
+	fill_in "session_email",    with: user.email.upcase
+	fill_in "session_password", with: user.password
+	click_button "Sign in"
+	cookies[:remember_token] = user.remember_token
+end
