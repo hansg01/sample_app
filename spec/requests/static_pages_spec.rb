fc7756e3d94a93a60e69a60d3b693a5abe466023
Hans Gogia <hans.gogia@gmail.com>
2013-06-28 13:37:38 +0530

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,13 +1,31 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-	let(:base_title) { "Gogia Sales" }
+  
   subject { page }
+  
   describe "Home Page" do
     before { visit root_path }
+    
     it { should have_selector('h1', text: 'Gogia Sales') }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector('title', text: '| Home') }
+    
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
 describe "Help page" do
   before { visit help_path }
